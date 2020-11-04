343d0fff7868d925777c44ba6a33d6d949429661
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-04 10:31:04 +0000

    App

---
+++ app.js
@@ -0,0 +1,5 @@
+let app = div().bc("#0f0").w(200).h(200).fl().aic().jcsa().a(
+	div().html("MongoDb test.").pad(10).bc("#ffa")
+)
+
+document.getElementById("root").appendChild(app.e)
--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT || "3000")
 
+app.use("/", express.static(__dirname))
+
 app.get('/', (req, res) => {
-	res.send('MongoDb test.')	
+	res.send(`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MongoDb Test</title>    
+    <script src="https://unpkg.com/@easychessanimations/foo/lib/fooweb.js"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+	<script src="app.js"></script>
+  </body>
+</html>
+`)
 })
 
 app.listen(port, () => {
