1e5bd7e32564a5392aba115e2c6945bc207c55a3
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-04 10:38:08 +0000

    Find one

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT || "3000")
 
+const MongoClient = require('mongodb').MongoClient
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+let collection = null
+
+client.connect(err => {
+	if(err){
+		console.log("MongoDb connection failed", err)
+	}else{
+		console.log("MongoDb connected!")
+		collection = client.db("sample_airbnb").collection("listingsAndReviews")		
+	}
+})
+
 app.use("/", express.static(__dirname))
+
+app.get('/findone', (req, res) => {
+	collection.findOne().then(doc => {
+		res.send("<pre>" + JSON.stringify(doc, null, 2) + "/<pre>")
+	})
+})
 
 app.get('/', (req, res) => {
 	res.send(`
@@ -14,6 +35,8 @@ app.get('/', (req, res) => {
     <script src="https://unpkg.com/@easychessanimations/foo/lib/fooweb.js"></script>
   </head>
   <body>
+	<a href="/findone" rel="noopener noreferrer" target="_blank">find one</a>
+	<hr>
     <div id="root"></div>
 	<script src="app.js"></script>
   </body>
