08c9a550dd230648907112bd925c7e810c15138c
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-18 14:20:25 +0000

    Styling

--- server.js
+++ server.js
@@ -321,7 +321,7 @@ app.get('/', (req, res) => {
   <body>
 
 	<div style="padding: 3px; background-color: #eee; margin-bottom: 6px;">
-		${req.user ? "logged in as <b>" + req.user.username + "</b> <a href='/logout'>log out</a>" : "<a href='/auth/lichess'>log in with lichess</a>"} 
+		${req.user ? "logged in as <b>" + req.user.username + "</b> | <a href='/logout'>log out</a>" : "<a href='/auth/lichess'>log in with lichess</a>"} 
 	| <a href="/?latest=true">view latest transactions</a> 
 	| <a href="/">home</a>
 	</div>
